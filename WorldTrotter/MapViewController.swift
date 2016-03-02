5e0738a4ebfa7438dcf1cbed0b6d66c1a328b91c
Sebastian <sebastian@sebastians-imac.local>
2016-03-01 21:00:09 -0300

    Added Map view and background now changes according to time  of day.

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Sebastian on 3/1/16.
+//  Copyright © 2016 Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
